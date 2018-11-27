8d34710184820555b193037a42cf1f2875afe28b
Victor <flarevictor@gmail.com>
2018-11-27 20:19:02 +0200

    first commit -m

---
+++ src/components/Rectangle/Rectangle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Circle extends Component {
+
+	render() {
+
+		return (
+			<div className="App">
+
+			</div>
+		);
+	}
+}
+
+export default Circle;
